d8ef75d54e62671cf266590c1e32f81a85ba222b
Vera Korzun <veraminich@gmail.com>
2021-04-20 14:15:05 +0200

    Adds changes to validationUser

--- server/api/users/users.js
+++ server/api/users/users.js
@@ -5,8 +5,8 @@ const { UsersCtrl } = require('../../controllers');
 const guard = require('../../utils/guard');
 const validateUser = require('../../utils/validations/validationUser');
 
-router.post('/auth/register', validateUser, UsersCtrl.reg);
-router.post('/auth/login', validateUser, UsersCtrl.login);
+router.post('/auth/register', validateUser.reg, UsersCtrl.reg);
+router.post('/auth/login', validateUser.login, UsersCtrl.login);
 router.post('/auth/logout', guard, UsersCtrl.logout);
 
 module.exports = router;
--- server/utils/validations/validationUser.js
+++ server/utils/validations/validationUser.js
@@ -1,21 +1,39 @@
 const Joi = require('joi');
 
-const validateUser = (req, res, next) => {
-  const schema = Joi.object({
-    name: Joi.string().alphanum().min(2).max(30),
-    email: Joi.string().email({ minDomainSegments: 2 }).required(),
-    password: Joi.string().min(6).max(20).required(),
-  });
-  const { error } = schema.validate(req.body);
+const schemaRegistration = Joi.object({
+  name: Joi.string().alphanum().min(2).max(30).required(),
+  email: Joi.string().email({ minDomainSegments: 2 }).required(),
+  password: Joi.string().min(6).max(20).required(),
+});
+
+const schemaLogin = Joi.object({
+  email: Joi.string().email({ minDomainSegments: 2 }).required(),
+  password: Joi.string().min(6).max(20).required(),
+});
+
+const validate = (schema, obj, next) => {
+  const { error } = schema.validate(obj);
   if (error) {
     const [{ message }] = error.details;
     return res.status(400).json({
       status: 400,
       message,
       data: 'Bad Request',
+      message: `Filed: ${message.replace(/"/g, '')}`,
     });
   }
   next();
 };
 
-module.exports = validateUser;
+const reg = (req, res, next) => {
+  return validate(schemaRegistration, req.body, next);
+};
+
+const login = (req, res, next) => {
+  return validate(schemaLogin, req.body, next);
+};
+
+module.exports = {
+  reg,
+  login,
+};
