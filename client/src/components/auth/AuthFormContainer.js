d3ec30605617b6fa4d21f33796ca6cef49b7ce8b
Oleg Dramaretskyi <dramarec@gmail.com>
2021-04-22 11:38:58 +0300

    added auth logic with google accaunt

--- client/src/components/auth/AuthFormContainer.js
+++ client/src/components/auth/AuthFormContainer.js
@@ -1,60 +1,58 @@
 import React from 'react';
-import { useLocation, useHistory } from 'react-router-dom';
+import { useLocation /* useHistory */ } from 'react-router-dom';
 import * as Yup from 'yup';
 import AuthForm from './AuthForm';
 
-
 const AuthFormContainer = () => {
+  const location = useLocation();
+  const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
+  const errorMessagesSchemaSignUp = Yup.object().shape({
+    username: Yup.string()
+      .min(2, 'Минимальное количество символов 2 ')
+      .max(20, 'Максимально допустимое количество символов 20')
+      .required('Введите имя'),
+    email: Yup.string()
+      .required('Введите email')
+      .matches(regex, 'Введен неверный email'),
 
-    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    password: Yup.string()
+      .min(8, 'Минимальное количество символов 8')
+      .max(20, 'Максимально допустимое количество символов 20')
+      .required('Введите пароль'),
+  });
 
-    const errorMessagesSchemaSignUp = Yup.object().shape({
-        username: Yup.string()
-            .min(2, 'Минимальное количество символов 2 ')
-            .max(20, 'Максимально допустимое количество символов 20')
-            .required('Введите имя'),
-        email: Yup.string()
-            .required('Введите email')
-            .matches(regex, 'Введен неверный email'),
+  const errorMessagesSchemaSignIn = Yup.object().shape({
+    email: Yup.string()
+      .required('Введите email')
+      .matches(regex, 'Введен неверный email'),
+    password: Yup.string()
+      .min(8, 'Минимальное количество символов 8')
+      .max(20, 'Максимально допустимое количество символов 20')
+      .required('Введите пароль'),
+  });
 
-        password: Yup.string()
-            .min(8, 'Минимальное количество символов 8')
-            .max(20, 'Максимально допустимое количество символов 20')
-            .required('Введите пароль'),
-    });
+  const errorMessagesSchema =
+    location.pathname === '/signup'
+      ? errorMessagesSchemaSignUp
+      : errorMessagesSchemaSignIn;
 
-    const errorMessagesSchemaSignIn = Yup.object().shape({
-        email: Yup.string()
-            .required('Введите email')
-            .matches(regex, 'Введен неверный email'),
-        password: Yup.string()
-            .min(8, 'Минимальное количество символов 8')
-            .max(20, 'Максимально допустимое количество символов 20')
-            .required('Введите пароль'),
-    });
+  const handleSubmit = values => {
+    // if (location.pathname === '/signup' && !age) {
+    // history.push('/');
 
-    const errorMessagesSchema =
-        location.pathname === '/signup'
-            ? errorMessagesSchemaSignUp
-            : errorMessagesSchemaSignIn;
+    return;
+    // }
+    // if (location.pathname === '/signup' && age) {
+    // signUp(values);
+    // } else signIn(values);
+  };
 
-    const handleSubmit = values => {
-        if (location.pathname === '/signup' && !age) {
-            history.push('/');
-
-            return;
-        }
-        if (location.pathname === '/signup' && age) {
-            signUp(values);
-        } else signIn(values);
-    };
-
-    return (
-        <AuthForm
-            handleSubmit={handleSubmit}
-            errorMessagesSchema={errorMessagesSchema}
-        />
-    );
- };
- export default AuthFormContainer;
+  return (
+    <AuthForm
+      handleSubmit={handleSubmit}
+      errorMessagesSchema={errorMessagesSchema}
+    />
+  );
+};
+export default AuthFormContainer;
---
+++ client/src/components/auth/GoogleAuthBtn.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+import axios from 'axios';
+
+const google = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
+const GoogleAuthBtn = () => {
+  const responseSuccessGoogle = response => {
+    axios({
+      method: 'POST',
+      url: 'http://localhost:5000/api/users/auth/google',
+      data: { tokenId: response.tokenId },
+    }).then(response => {
+      console.log('Google login sucess ===> response', response);
+    });
+  };
+
+  const responseErrorGoogle = response => {
+    console.log('responseErrorGoogle ===> response', response);
+  };
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId={google}
+        buttonText="Google"
+        onSuccess={responseSuccessGoogle}
+        onFailure={responseErrorGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
+    </div>
+  );
+};
+
+export default GoogleAuthBtn;
