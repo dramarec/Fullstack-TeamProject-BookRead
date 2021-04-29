bf097cf95676c8f998b7ba4612b231e01f3fbada
Vera Korzun <veraminich@gmail.com>
2021-04-29 16:00:14 +0200

    Adds changes to styles

--- client/src/components/library/readBooks/ReadBookItemStyled.js
+++ client/src/components/library/readBooks/ReadBookItemStyled.js
@@ -1,81 +1,81 @@
 import styled from 'styled-components';
 
 const ReadBookItemStyled = styled.li`
-display: flex;
-align-items: center;  
-font-family: 'Montserrat-Medium';
-font-size: 14px;
-background-color: #FFFFFF;
-box-shadow: 0px 2px 3px rgba(9, 30, 63, 0.1);
-margin-bottom: 10px;
-@media screen and (min-width: 768px) {
-    padding: 11px 20px;;
-  }
-  @media screen and (min-width: 1280px) {
-    padding: 11px 0;
-    padding-left: 20px;
-    }
-.bookName {
     display: flex;
-    align-items: center;  
-    flex: 2 1 0%;
-}
-.star-ratings {
-    margin-right: 18px;
-}
-.star-container svg {
-    stroke: #FF6B08;
-}
-.bookName img {
-    margin-right: 18px; 
-}
-p {
-    @media screen and (min-width: 768px) {
-        flex: 1 1 0%; 
-      }
-}
-.pages {
-    @media screen and (min-width: 768px) {
-        flex: 0.6 1 0%; 
-      }
-    @media screen and (min-width: 1280px) {
-        flex: 1 1 0%; 
-    }
-    }
-.publishYear {
-    @media screen and (min-width: 768px) {
-        flex: 0.6 1 0%; 
-        }
-    @media screen and (min-width: 1280px) {
-        flex: 1 1 0%; 
-        }
-    }
-.review{
-    display: flex;
-    flex: 2 1 0%;
     align-items: center;
-}
-
-.review-button {
-    //padding: 11px 36px;
-    background-color: #6D7A8D;
     font-family: 'Montserrat-Medium';
-    color: #FFFFFF;
     font-size: 14px;
-    border: none;
-
+    background-color: #ffffff;
+    box-shadow: 0px 2px 3px rgba(9, 30, 63, 0.1);
+    margin-bottom: 10px;
     @media screen and (min-width: 768px) {
-      padding: 11px 11px;;
+        padding: 11px 20px;
     }
     @media screen and (min-width: 1280px) {
-        padding: 11px 36px;
-      }
-}
+        padding: 11px 0;
+        padding-left: 20px;
+    }
+    .bookName {
+        display: flex;
+        align-items: center;
+        flex: 2 1 0%;
+    }
+    .star-ratings {
+        margin-right: 18px;
+    }
+    .star-container svg {
+        stroke: #ff6b08;
+    }
+    .bookName img {
+        margin-right: 18px;
+    }
+    p {
+        @media screen and (min-width: 768px) {
+            flex: 1 1 0%;
+        }
+    }
+    .pages {
+        @media screen and (min-width: 768px) {
+            flex: 0.6 1 0%;
+        }
+        @media screen and (min-width: 1280px) {
+            flex: 1 1 0%;
+        }
+    }
+    .publishYear {
+        @media screen and (min-width: 768px) {
+            flex: 0.6 1 0%;
+        }
+        @media screen and (min-width: 1280px) {
+            flex: 1 1 0%;
+        }
+    }
+    .review {
+        display: flex;
+        flex: 2 1 0%;
+        align-items: center;
+    }
 
-.star {
-    display: flex;
-    align-items: center;  
-}
-`
+    .review-button {
+        //padding: 11px 36px;
+        background-color: #6d7a8d;
+        font-family: 'Montserrat-Medium';
+        color: #ffffff;
+        font-size: 14px;
+        border: none;
 
-export default ReadBookItemStyled
+        @media screen and (min-width: 768px) {
+            padding: 11px 11px;
+        }
+        @media screen and (min-width: 1280px) {
+            padding: 11px 36px;
+        }
+    }
+
+    .star {
+        display: flex;
+        align-items: center;
+    }
+`;
+
+export default ReadBookItemStyled;
--- client/src/components/library/readBooks/ReadBooksStyled.js
+++ client/src/components/library/readBooks/ReadBooksStyled.js
@@ -1,55 +1,59 @@
 import styled from 'styled-components';
 
 const ReadBooksStyled = styled.section`
-background-color: #F6F7FB;
-font-size: 14px;
-margin-bottom: 30px;
-
-h2 {
-    line-height: 38px;
-    font-family: 'Montserrat-SemiBold';
-    font-size: 19px;
-}
-.table-title {
-    font-family: 'Montserrat-Medium';
+    background-color: #f6f7fb;
     font-size: 14px;
-    display: flex;
-    line-height: 38px;
-    color: #898F9F;
-}
-.table-title p:nth-child(1) {
-    flex: 2 1 0%;
-}
-.table-title p:nth-child(2) {
-    flex: 1 1 0%;
-}
-.table-title p:nth-child(3) {
-    @media screen and (min-width: 768px) {
-        flex: 0.6 1 0%; 
-      }
-    @media screen and (min-width: 1280px) {
-        flex: 1 1 0%; 
+    margin-bottom: 30px;
+
+    .readMobil {
+        display: flex;
+        flex-direction: column;
+        width: 270px;
+        margin: 0 auto;
     }
-}
-.table-title p:nth-child(4) {
-    @media screen and (min-width: 768px) {
-        flex: 0.6 1 0%; 
-      }
-    @media screen and (min-width: 1280px) {
-        flex: 1 1 0%; 
+
+    h2 {
+        line-height: 38px;
+        font-family: 'Montserrat-SemiBold';
+        font-size: 19px;
     }
-}
-.table-title p:nth-child(5) {
-    @media screen and (min-width: 768px) {
-        flex: 2 1 0%; 
-      }
-    @media screen and (min-width: 1280px) {
-        flex: 2 1 0%; 
+    .table-title {
+        font-family: 'Montserrat-Medium';
+        font-size: 14px;
+        display: flex;
+        line-height: 38px;
+        color: #898f9f;
     }
-}
+    .table-title p:nth-child(1) {
+        flex: 2 1 0%;
+    }
+    .table-title p:nth-child(2) {
+        flex: 1 1 0%;
+    }
+    .table-title p:nth-child(3) {
+        @media screen and (min-width: 768px) {
+            flex: 0.6 1 0%;
+        }
+        @media screen and (min-width: 1280px) {
+            flex: 1 1 0%;
+        }
+    }
+    .table-title p:nth-child(4) {
+        @media screen and (min-width: 768px) {
+            flex: 0.6 1 0%;
+        }
+        @media screen and (min-width: 1280px) {
+            flex: 1 1 0%;
+        }
+    }
+    .table-title p:nth-child(5) {
+        @media screen and (min-width: 768px) {
+            flex: 2 1 0%;
+        }
+        @media screen and (min-width: 1280px) {
+            flex: 2 1 0%;
+        }
+    }
+`;
 
-
-
-`
-
-export default ReadBooksStyled
+export default ReadBooksStyled;
